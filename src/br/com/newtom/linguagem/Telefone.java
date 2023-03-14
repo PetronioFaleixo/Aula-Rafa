db6692f2bfd896bb8e4c88d43a0c301250171ce1
12115423 <petroniofaleixo@gmail.com>
2023-03-14 19:30:05 -0300

    aula14/03

--- src/br/com/newtom/linguagem/Telefone.java
+++ src/br/com/newtom/linguagem/Telefone.java
@@ -5,4 +5,18 @@ public class Telefone {
     Contato contato2, contato3;
     public String cor;
     public String marca;
+
+    public void Tocar(String estiloMusica){
+        System.out.print("Tocando" + estiloMusica);
+    }
+    public void Ligar(Contato contato)
+    {
+        if(cor.equals("Preto"))
+        {
+            if(contato.nome.equals("Juliana")){
+                System.out.print("Liando para Julia");
+            }
+        }
+
+    }
 }
